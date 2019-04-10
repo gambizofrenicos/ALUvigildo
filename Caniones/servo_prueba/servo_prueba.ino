f654c1e05625b0888b7131546ab9b9c745e0cc26
montse-dcd <montse.dcd@gmail.com>
2019-04-10 17:44:51 +0200

    Create servo_prueba.ino

---
+++ Caniones/servo_prueba/servo_prueba.ino
@@ -0,0 +1,13 @@
+#include <Servo.h>
+
+Servo myservo;  // create servo object to control a servo
+
+void setup() {
+  myservo.attach(11);  
+}
+
+void loop() {
+  myservo.write(90);                  // sets the servo position according to the scaled value
+  delay(15);                           // waits for the servo to get there
+}
+
