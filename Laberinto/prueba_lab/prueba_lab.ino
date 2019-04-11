8e60ba5d950daf37b84d28b46c58b4888f0a1587
Chechu <chechupareja@gmail.com>
2019-04-11 18:34:12 +0200

    11/04. Ajustado algun que otro pid y tocada alguna cosilla

--- Laberinto/prueba_lab/prueba_lab.ino
+++ Laberinto/prueba_lab/prueba_lab.ino
@@ -1,6 +1,10 @@
-#define PWM 40 // PWM "base" a los motores (al que queremos que vayan)
-#define MAX_PWM 150 // Limitacion superior de PWM
-#define MIN_PWM 0 // Limitacion inferior de PWM
+#include "encoder.h"
+#include "pid.h"
+#include "motores.h"
+
+#define PWM 60 // PWM "base" a los motores (al que queremos que vayan)
+#define MAX_PWM 80 // Limitacion superior de PWM
+#define MIN_PWM 40 // Limitacion inferior de PWM
 
 // Pines motor izquierdo + encoder (malito)
 #define ENC1_MOTI 2
@@ -22,33 +26,67 @@ float dist_i;
 float dist_d;
 
 void setup() {
- // Inicializamos motor I
+  // Inicializamos motor I
   pinMode(ENC1_MOTI, INPUT);
   pinMode(ENC2_MOTI, INPUT);
   pinMode(PWMI, OUTPUT);
   pinMode(DIRI, OUTPUT);
-  
-   // Inicializamos motor D
+
+  // Inicializamos motor D
   pinMode(ENC1_MOTD, INPUT);
   pinMode(ENC2_MOTD, INPUT);
   pinMode(PWMD, OUTPUT);
   pinMode(DIRD, OUTPUT);
+  // initialize hardware interrupts
+  attachInterrupt(0, EncoderEventMotI, CHANGE);
+  attachInterrupt(1, EncoderEventMotD, CHANGE);
 
+  Serial.begin(9600);
 }
 
 void loop() {
-  dist_i = 2076.0/(analogRead(SHARPI)-11.0);
-  dist_d = 2076.0/(analogRead(SHARPO)-11.0);
+  girar90D_coche();
+  para();
+  delay(2000);
+  /*dist_i = 2076.0 / (analogRead(SHARPI) - 11.0);
+  dist_d = 2076.0 / (analogRead(SHARPO) - 11.0);
   digitalWrite(DIRI, HIGH);
   digitalWrite(DIRD, LOW);
-  if ((dist_i < 8)){
-    analogWrite(PWMI, PWM);
-    } else {
-      analogWrite(PWMI, MIN_PWM);
-      }
-  if ((dist_d < 8)){
-    analogWrite(PWMD, PWM);
-    } else {
-      analogWrite(PWMD, MIN_PWM);
-      }
+   if (dist_d > 15){
+    //Giro dcha
+    para();
+    delay(1000);
+    avanza_mm(40);
+    para();
+    delay(1000);
+    girar90D();
+    para();
+    delay(1000);
+    avanza_mm(50);
+  } else if (dist_i > 15){
+    //Giro Izda
+    para();
+    delay(1000);
+    avanza_mm(40);
+    para();
+    delay(1000);
+    girar90I();
+    para();
+    delay(1000);
+    avanza_mm(50);
+  } else if (!digitalRead(SHARPA)){
+    //Media vuelta
+    para();
+    delay(1000);
+    girar90D();
+    girar90D();
+    para();
+    delay(1000);
+    avanza_mm(50);
+  } else {
+    error(dist_i, dist_d);
+    pwmi = PWM - PID_lab;
+    pwmd = PWM + PID_lab;
+    avanzar();
+    }*/
 }
