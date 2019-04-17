e022978ca54a1a4af23d6f014af266afe0b4e421
JLBicho <jlmv.96@gmail.com>
2019-04-17 17:58:39 +0200

    Cambios de orden necesario
    jfilasgiabgiñorabenioñbnñbnvñbnbio´josi manda

--- Dragon/US_dragon/US_dragon.ino
+++ Dragon/US_dragon/US_dragon.ino
@@ -1,4 +1,4 @@
-#define PIN_TRIGGER_1 8
+#define PIN_TRIGGER_1 7
 #define PIN_ECHO_2 A0
 #define ESPERA_ENTRE_EMISION 3 // tiempo entre lecturas consecutivas en milisegundos
 
@@ -7,7 +7,7 @@ unsigned long reloj = 0; // reloj para calcular la media cada 10ms
 float media = 0; // calcula la media de las mediciones
 unsigned int n = 0; // cuenta el numero de mediciones
 long duration = 0;
-float media_max = 0, media_min = 1023;
+float media_max = 0, media_min = 10;
 void setup()
 {
   Serial.begin(9600);
@@ -20,50 +20,7 @@ void setup()
 }
 
 void loop() {
-<<<<<<< HEAD
 
-  // EMITIENDO DE 1
-  /* if (millis() - cronometro > ESPERA_ENTRE_EMISION) {
-     for (int i = 0; i < 3; i++) {
-       digitalWrite(PIN_TRIGGER_1, HIGH); // Un pulso a nivel alto…
-       delayMicroseconds(10); // …durante 10 µs y
-       digitalWrite(PIN_TRIGGER_1, LOW);
-       delayMicroseconds(10); // …volver al nivel bajo
-     }
-     cronometro = millis();
-    }*/
-  digitalWrite(PIN_TRIGGER_1, HIGH); // Un pulso a nivel alto…
-  delayMicroseconds(10); // …durante 10 µs y
-  digitalWrite(PIN_TRIGGER_1, LOW);
-  delayMicroseconds(30); // …volver al nivel bajo
-
-  // LEE DE 2
-  /*  media += analogRead(PIN_ECHO_2);
-    n++;
-    if (millis() - reloj > 10) {
-      media = media / n;
-      Serial.println(analogRead(PIN_ECHO_2));
-      if (media < 2.5) { // si la media es baja, tiene muchos 0 --> detecta pulso
-        digitalWrite(13, HIGH); // enciende el LED
-      } else { // si la media es alta, tiene pocos 0 --> no detecta pulso
-        digitalWrite(13, LOW); // enciende el LED
-      }*/
-
-  /*  Serial.print("media:");
-     Serial.print("\t");
-     Serial.print(media);
-     Serial.print("\t\t");
-     Serial.print("echo:");
-     Serial.print("\t");
-     Serial.println(analogRead(PIN_ECHO_2));*/
-
-  /*  media = 0;
-    n = 0;
-    reloj = millis();*/
-  //}
-
-
-=======
   /*
     // EMITIENDO DE 1
     if (millis() - cronometro > ESPERA_ENTRE_EMISION) {
@@ -80,16 +37,21 @@ void loop() {
 
   if (millis() - reloj > 10) {
     media = media / n;
+    if (millis() - cronometro > 100) {
+      media_max = media;
+      media_min = media;
+      cronometro = millis();
+    }
     if (media > media_max) {
       media_max = media;
     } else if (media < media_min) {
       media_min = media;
     }
-    Serial.print(media);
-    Serial.print(" ");
-    Serial.print(media_max);
-    Serial.print(" ");
-    Serial.println(media_min);
+    Serial.println(media);
+    //Serial.print(" ");
+    //Serial.print(media_max);
+    //Serial.print(" ");
+    //Serial.println(media_min);
     if (media < (media_max + media_min) / 2) { // si la media es baja, tiene muchos 0 --> detecta pulso
       digitalWrite(13, HIGH); // enciende el LED
     } else { // si la media es alta, tiene pocos 0 --> no detecta pulso
@@ -99,10 +61,5 @@ void loop() {
     n = 0;
     reloj = millis();
   }
-  if (millis() - cronometro > 5000) {
-    media_max = 0;
-   // media_min = 1023;
-    cronometro = millis();
-  }
->>>>>>> c494357e7e5044c74f22a1066466b1aa726c2c69
+
 }
