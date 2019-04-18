ae338a144652e9ab222ea1058605d8d56647d975
eslugapf2 <luciagg-e@hotmail.com>
2019-04-18 12:21:05 +0200

    Update US_dragon.ino

--- Dragon/US_dragon/US_dragon.ino
+++ Dragon/US_dragon/US_dragon.ino
@@ -1,5 +1,6 @@
-#define PIN_TRIGGER_1 8
-#define PIN_ECHO_2 A0
+#define PIN_TRIGGER_1 22
+#define PIN_TRIGGER_2 7
+#define PIN_ECHO_2 8
 #define ESPERA_ENTRE_EMISION 3 // tiempo entre lecturas consecutivas en milisegundos
 
 unsigned long cronometro = 0; // cronometro para emitir cada 3ms --> ESTO NO LO TENDREMOS
@@ -12,7 +13,10 @@ void setup()
 {
   Serial.begin(9600);
   pinMode(PIN_TRIGGER_1, OUTPUT);
+
+  pinMode(PIN_TRIGGER_2, OUTPUT);
   pinMode(PIN_ECHO_2, INPUT);
+
   pinMode(13, OUTPUT);
   digitalWrite(PIN_TRIGGER_1, LOW);
   digitalWrite(13, LOW);
@@ -25,16 +29,13 @@ void loop() {
   // EMITIENDO DE 1
 
   digitalWrite(PIN_TRIGGER_1, HIGH); // Un pulso a nivel alto…
-  delayMicroseconds(10); // …durante 10 µs y
-  digitalWrite(PIN_TRIGGER_1, LOW);
-  delayMicroseconds(30); // …volver al nivel bajo
 
-/*
+
   // LEE DE 2
-  media += analogRead(PIN_ECHO_2);
-  n++;
+  /*media += analogRead(PIN_ECHO_2);
+    n++;
 
-  if (millis() - reloj > 10) {
+    if (millis() - reloj > 10) {
     media = media / n;
     if (media > media_max) {
       media_max = media;
@@ -55,21 +56,30 @@ void loop() {
     media = 0;
     n = 0;
     reloj = millis();
-  }
-  if (millis() - cronometro > 5000) {
+    }
+    if (millis() - cronometro > 5000) {
     media_max = 0;
-   // media_min = 1023;
+    // media_min = 1023;
     cronometro = millis();
-  }
-*/
+    }*/
+
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(5);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  duration = pulseIn(echoPin, HIGH);
+  Serial.println(duration);
+
 }
 
 /*int promedio(int N)
-{
+  {
   long suma=0;
   for(int i=0;i<N;i++)
   {
     suma=suma+analogRead(A0);
-  }  
+  }
   return(suma/N);
-}*/
+  }*/
