c5eff286acd7d6128d516d87cfcdc67bc1aa5575
chechugador <44680450+chechugador@users.noreply.github.com>
2019-04-23 00:02:10 +0200

    pues eso, pruebas de siguelineas

---
+++ Siguelineas/Siguelineas_prueba1/motores.h
@@ -0,0 +1,133 @@
+// PWM
+#define PWM 95 // PWM "base" a los motores (al que queremos que vayan)
+#define MAX_PWM 120 // Limitacion superior de PWM
+#define MIN_PWM 20 // Limitacion inferior de PWM
+
+
+// Giro
+#define G90I 130
+#define G90D -100
+
+// Pulsos por vuelta
+#define PPV 180
+
+// Motor izquierdo
+#define PWMI 5
+#define DIRI 7
+
+//Motor derecho
+#define PWMD 6
+#define DIRD 8
+
+float pwmi = 0, pwmd = 0; // pwm que pasamos a cada motor
+
+void acotar();
+void arrancar();
+void avanzar();
+void girar90I();
+void girar90D();
+void girar90D_coche();
+void para();
+void avanza_mm(float d);
+
+//Arrancar sin caballito
+void arrancar(int e1, int e2) {
+
+  digitalWrite(DIRI, HIGH);
+  digitalWrite(DIRD, LOW);
+
+  for (int i = MIN_PWM; i <= PWM; i++) {
+
+    error(e1, e2);
+
+    pwmi = i - e;
+    pwmd = i + e;
+
+    acotar();
+
+    analogWrite(PWMI, pwmi);
+    analogWrite(PWMD, pwmd);
+    delay(10);
+  }
+}
+
+// Acotar el pwm de los motores para que no se salga del MIN y MAX
+void acotar() {
+  if (pwmi > MAX_PWM) {
+    pwmi = MAX_PWM;
+  } else if (pwmi < MIN_PWM) {
+    pwmi = MIN_PWM;
+  }
+
+  if (pwmd > MAX_PWM) {
+    pwmd = MAX_PWM;
+  } else if (pwmd < MIN_PWM) {
+    pwmd = MIN_PWM;
+  }
+}
+
+void avanzar() {
+  digitalWrite(DIRI, HIGH);
+  digitalWrite(DIRD, LOW);
+
+  analogWrite(PWMI, pwmi);
+  analogWrite(PWMD, pwmd);
+}
+
+void girar90I() {
+  CountD = 0;
+  digitalWrite(DIRI, LOW);
+  digitalWrite(DIRD, LOW);
+  while (G90I > CountD) {
+    analogWrite(PWMI, PWM);
+    analogWrite(PWMD, PWM);
+  }
+}
+
+void girar90D() {
+  CountD = 0;
+  digitalWrite(DIRI, HIGH);
+  digitalWrite(DIRD, HIGH);
+  while (G90D < CountD) {
+    analogWrite(PWMI, PWM);
+    analogWrite(PWMD, PWM);
+  }
+}
+
+void girar90D_coche(){
+  CountI = 0;
+  digitalWrite(DIRI, HIGH);
+  digitalWrite(DIRD, HIGH);
+  while (281 > CountI) {
+    Serial.println(CountI);
+    analogWrite(PWMI, PWM);
+    analogWrite(PWMD, 0);
+  }
+}
+
+void para() {
+  analogWrite(PWMI, 0);
+  analogWrite(PWMD, 0);
+}
+
+void avanza_mm(float d) {
+  int encI = CountI;
+  int encD = CountD;
+
+  d = (d/(2*PI*16)) * PPV; //16 mm es el radio de las ruedas de pololu
+  error_mm(d,((CountI + CountD)/2) - ((encI + encD)/2));
+  
+  //while ((((CountI + CountD) / 2) - ((encI + encD) / 2)) < d) {
+  while ((e_mm >= 1) || (e_mm <= -1)){
+    
+  error(CountI, CountD);
+	error_mm(d,((CountI + CountD)/2) - ((encI + encD)/2));
+
+    pwmi = PID_mm - PID;
+    pwmd = PID_mm + PID;
+
+    acotar();
+    avanzar();
+  }
+
+}
---
+++ Siguelineas/Siguelineas_prueba2/Siguelineas_prueba2.ino
@@ -0,0 +1,88 @@
+#include "pid.h"
+#include "encoder.h"
+#include "motores.h"
+#include <QTRSensors.h>
+#include <Math.h>
+
+#define PWM_line 20
+#define DETECTA_META sensorValues[7] > VALOR_UMBRAL
+#define DETECTA_CURVA sensorValues[6] > VALOR_UMBRAL
+#define VA_RECTO (sensorValues[2] > VALOR_UMBRAL) && (sensorValues[3] > VALOR_UMBRAL)
+
+/****BANDERAS DE PARADA Y SALIDA****/
+int start = 0;
+bool parada = false;
+
+/*******Para las curvitas******/
+float reset_curva = 0; //Lo que le sumamos al pwm base (cuando es 0 va lento)
+
+const int vmin=80;
+const int vmax=150;
+
+
+/* QTR-8RC*/
+// Configuracion inicial del sensor
+#define NUM_SENSORS 8 // numero de sensores
+#define TIMEOUT 2500 // tiempo de espera 
+#define EMITTER_PIN 2 // en teoría no hace falta (es para apagar los leds)
+//Del A0 al A5 son los 6 pines delanteros que siguen la linea.
+//La alita I (cambios de curvatura) es el pin 10.
+//La alita D (start/end) es el pin 13.
+QTRSensorsRC qtrrc((unsigned char[]) {
+  A0, A1, A2, A3, A4, A5, 10, 9
+}, NUM_SENSORS, TIMEOUT, EMITTER_PIN); // Sensor QRT y pines asociados
+unsigned int sensorValues[NUM_SENSORS]; // Array para guardar los valores de los sensores
+
+/* CONSTANTES */
+#define VALOR_UMBRAL 500 // Valor umbral que determina si hay linea o no (hay que comprobar)
+
+/* PID */
+float errores[NUM_SENSORS] = { -5.0, -3.0, -1.0, 1.0, 3.0, 5.0, 0.0, 0.0}; // valor del error asignado a cada sensor. Las alitas no suman error
+
+void setup() {
+  /* Serial */
+  Serial.begin(9600);
+  Serial.println("Inicializando...");
+  /* Motores */
+  pinMode(DIRD, OUTPUT);
+  pinMode(DIRI, OUTPUT);
+  pinMode(PWMD, OUTPUT);
+  pinMode(PWMI, OUTPUT);
+
+  /* Calibración */
+  //calibracion(); // calibracion del sensor y muestra por serial los datos
+
+}
+
+void loop() {
+  /* Calculo del error */
+
+  qtrrc.read(sensorValues);
+
+  for (int i = 0; i < (NUM_SENSORS - 2); i++) {
+    e_line += errores[i] * sensorValues[i];
+  }
+
+  error_line(); //Gestiona el error
+  vbase = vmin+(vmax-vmin)*exp(-Kv*abs(Kp_line*e_line));
+
+  if (!parada) {
+    pwmd = vbase - PID_line;
+    pwmi = vbase + PID_line;
+    acotar();
+  }
+
+  else {
+    pwmd = 0;
+    pwmi = 0;
+  }
+
+
+  //    Serial.print("PWMs:  "); Serial.print(pwmd); Serial.print("\t"); Serial.println(pwmi);
+  //    Serial.print("out:  "); Serial.println(out);
+  //    Serial.print("e_line:  "); Serial.println(e_line);
+  //    Serial.print("start:  "); Serial.println(start);
+  //    Serial.print("sensorD:  "); Serial.println(sensorValues[7]);
+  //    delay(500);
+
+}
